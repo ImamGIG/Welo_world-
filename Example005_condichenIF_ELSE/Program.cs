ad0e7e0ed3b2d65ad9313a911a02cfec96580f27
Imam Gig <rinoneo666@gmail.com>
2023-06-07 21:25:46 +0300

    add IF ESLE

---
+++ Example005_condichenIF_ELSE/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Имам")
+
+{
+    Console.WriteLine("Ура это же маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
